38e23c78219a4ebcb4c2a8dc4a2061673a7f2b57
l4na-git <tagrana0711@gmail.com>
2026-09-14 13:23:01 +0900

    feat: ポートフォリオのOGPメタデータを追加する

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://www.l4na.com',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
